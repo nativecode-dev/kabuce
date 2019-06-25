47e7b8f84054b325c52da13b821e3c5ac3cc2524
Mike Pham <mike.pham@proplogix.com>
2019-06-25 10:25:25 -0400

    chore: adding projects

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kabuce
+
+## Demo
+
+A demonstration deploy is available at https://nativecode-dev.github.io/kabuce/.
+- Register for an account
+- All users have administrative access
---
+++ apps/kabuce/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>kabuce</div>
+        <div>
+          (<a href="https://www.youtube.com/watch?v=du38zTteC6U">kuh·boos</a>)
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
