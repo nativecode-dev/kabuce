06afecbbb4147cac74c5587c65b333f485cb6de5
Mike Pham <reginfo@nativecode.com>
2020-09-12 23:33:30 -0400

    chore: update ci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PATH=node_modules/.bin:$PATH
+
 if [ "$DOCKER_USERNAME" = "" ] || [ "$DOCKER_TOKEN" = "" ]; then
   echo "You must provide both a DOCKER_USERNAME and DOCKER_TOKEN environment variables."
   exit 1
@@ -9,21 +11,22 @@ fi
 
 echo -n $DOCKER_TOKEN | docker login -u $DOCKER_USERNAME --password-stdin
 
-for PROJECT in $(cat .changed); do
-  echo "$PROJECT changed."
+for PROJECT_PATH in $(lerna list --json | jq -r '.[].location'); do
+  PROJECT=$(basename $PROJECT_PATH)
+  echo $PROJECT
 
-  if [ ! -f packages/$PROJECT/package.json ]; then
+  if [ ! -f $PROJECT_PATH/package.json ]; then
     continue
   fi
 
   # Define image names and default tag.
   IMAGE_NAME=nativecode/${PROJECT}
   PROJECT_DIR=packages/${PROJECT}
-  PACKAGE_NAME=$(cat packages/$PROJECT/package.json | jq -r '.name')
-  VERSION=$(cat packages/$PROJECT/package.json | jq -r '.version')
+  PACKAGE_NAME=$(cat $PROJECT_PATH/package.json | jq -r '.name')
+  VERSION=$(cat $PROJECT_PATH/package.json | jq -r '.version')
 
   # Build image and apply version tag
-  docker build -f packages/$PROJECT/Dockerfile -t ${IMAGE_NAME} packages/$PROJECT || exit 1
+  docker build -f $PROJECT_PATH/Dockerfile -t ${IMAGE_NAME} $PROJECT_PATH || exit 1
   docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${VERSION} || exit 1
   docker push ${IMAGE_NAME}:${VERSION} || exit 1
 
